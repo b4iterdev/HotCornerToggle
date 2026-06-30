bb25ccd0899d04d8eabb569d48725873f090ddc1
b4iterdev <thaimeo200389@gmail.com>
2026-06-30 16:17:28 +0700

    Initial Commit

---
+++ Hot Corner Toggle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hot Corner Toggle
+//
+//  Created by Nguyen Minh Thai on 30/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hot Corner Toggle/Hot_Corner_ToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hot_Corner_ToggleApp.swift
+//  Hot Corner Toggle
+//
+//  Created by Nguyen Minh Thai on 30/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Hot_Corner_ToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
